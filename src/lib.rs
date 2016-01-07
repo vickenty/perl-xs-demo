5681a3acbc8456f7d9136970c263e790838ac224
Vickenty Fesunov <kent@setattr.net>
2016-01-07 23:48:40 +0100

    Update.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,22 @@
 #[macro_use]
 extern crate perl_xs;
 
-XS! {
-    package XSDemo (boot_XSDemo) {
-        sub hello(xs) {
-            xs.prepush();
-            xs.push_string("Hello from Rust XS demo");
-            xs.putback();
-        }
-    }
+use perl_xs::{ Context };
+use perl_xs::raw::{ PerlContext, IV, CV };
+
+#[no_mangle]
+pub extern "C" fn hello(pthx: PerlContext, _: *mut CV) {
+    let mut ctx = Context::new(pthx);
+    ctx.prepush();
+    ctx.push("Hello from Rust XS demo");
+    ctx.putback();
+}
+
+#[no_mangle]
+pub extern "C" fn boot_XSDemo(pthx: PerlContext, _: *mut CV) {
+    let mut ctx = Context::new(pthx);
+    ctx.new_xs("XSDemo::hello", hello);
+    ctx.prepush();
+    ctx.push(1 as IV);
+    ctx.putback();
 }
