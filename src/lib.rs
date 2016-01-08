2752329fc68e957b1af0d1e250825c642b77b94b
Vickenty Fesunov <kent@setattr.net>
2016-01-08 02:20:43 +0100

    Use new perl-xs macro.

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,14 @@
 #[macro_use]
 extern crate perl_xs;
 
-use perl_xs::{ Context };
-use perl_xs::raw::{ PerlContext, IV, CV };
+XS! {
+    package XSDemo {
+        sub hello (ctx) {
+            ctx.prepush();
+            ctx.push("Hello from Rust XS demo");
+            ctx.putback();
+        }
+    }
 
-#[no_mangle]
-pub extern "C" fn hello(pthx: PerlContext, _: *mut CV) {
-    let mut ctx = Context::new(pthx);
-    ctx.prepush();
-    ctx.push("Hello from Rust XS demo");
-    ctx.putback();
-}
-
-#[no_mangle]
-pub extern "C" fn boot_XSDemo(pthx: PerlContext, _: *mut CV) {
-    let mut ctx = Context::new(pthx);
-    ctx.new_xs("XSDemo::hello", hello);
-    ctx.prepush();
-    ctx.push(1 as IV);
-    ctx.putback();
+    loader boot_XSDemo;
 }
