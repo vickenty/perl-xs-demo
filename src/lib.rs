76e5299e39b97e3149365fd36560e1c70f12accf
Vickenty Fesunov <kent@setattr.net>
2016-01-10 16:02:11 +0100

    Try out new SV handles.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,19 @@
 #[macro_use]
 extern crate perl_xs;
 
+use perl_xs::{ SV, IV, Scalar };
+
 XS! {
     package XSDemo {
         sub hello (ctx) {
             xs_return!(ctx, "Hello from Rust XS demo");
+        }
+
+        sub add_sub (ctx) {
+            let a: IV = ctx.st_fetch(0);
+            let v: SV = ctx.st_fetch(1);
+            let b = v.to_iv();
+            xs_return!(ctx, a + b, a - b);
         }
     }
 
--- t/simple.t
+++ t/simple.t
@@ -6,5 +6,8 @@ use Test::More;
 require_ok("XSDemo");
 
 is XSDemo::hello(), "Hello from Rust XS demo", "greeting";
+my ($sum, $diff) = XSDemo::add_sub(3, 4);
+is $sum, 7, "add_sub added";
+is $diff, -1, "add_sub subtracted";
 
 done_testing;
